04ac7d6e1381febd53695cf992e566388912d20e
kbu715 <kbu715@gmail.com>
2020-12-03 00:04:51 +0900

    finishing up back-end

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,51 @@
+const { AuthenticationError, UserInputError } = require('apollo-server');
+
+const checkAuth = require('../../util/check-auth');
+const Post = require('../../models/Post');
+
+module.exports = {
+  Mutation: {
+    createComment: async (_, { postId, body }, context) => {
+      const { username } = checkAuth(context);
+      if (body.trim() === '') {
+        throw new UserInputError('Empty comment', {
+          errors: {
+            body: 'Comment body must not empty'
+          }
+        });
+      }
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        post.comments.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString()
+        });
+        await post.save();
+        return post;
+      } else throw new UserInputError('Post not found');
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+          //findIndex() 메서드는 주어진 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 인덱스를 반환합니다. 만족하는 요소가 없으면 -1을 반환합니다.
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1); //splice(index, 1) : index부터 1개 삭제
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
+      } else {
+        throw new UserInputError('Post not found');
+      }
+    }
+  }
+};
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,21 @@
 const postsResolvers = require('./posts');
 const usersResolvers = require('./users');
+const commentsResolvers = require('./comments');
 
 module.exports = {
+    Post: { // Query나 Mutation이 Post를 return 하면 이 modifier를 통해 likeCount와 commentCount가 생긴다!!!
+        likeCount: (parent) => parent.likes.length,
+        commentCount: (parent) => parent.comments.length
+    },
     Query: {
         ...postsResolvers.Query
     },
     Mutation: {
         ...usersResolvers.Mutation,
-        ...postsResolvers.Mutation
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
+    },
+    Subscription: {
+        ...postsResolvers.Subscription
     }
 }
