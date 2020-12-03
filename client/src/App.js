73e35cdfc28658eb21364322aa8addbfe52e6108
kbu715 <kbu715@gmail.com>
2020-12-03 21:04:26 +0900

    router setting & semantic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
+import { Container } from "semantic-ui-react";
 
 function App() {
   return (
-    <div>
-      <h1>Hi world!!!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
